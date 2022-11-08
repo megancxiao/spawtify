e07e9ef7166792c344036a415382bfc9b66ed8bb
megancxiao <megancxiao@gmail.com>
2022-11-07 18:39:14 -0600

    update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var expressSession = require('express-session');
 
 
 var index = require('./routes/index');
-// var users = require('./routes/users');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -32,7 +32,7 @@ app.use(expressSession({
 }));
 
 app.use('/', index);
-// app.use('/users', users);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
